1ab98da6f6825093e17a7f1cfbc67c929cbdd209
ashkan karimi <karimiashkan8186@gmail.com>
2021-09-21 02:32:08 +0430

    init

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if config("DEBUG", cast=bool):
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
